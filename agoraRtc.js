e5ed60e2ceee2c5d2b5471cb93b50aee55f77c11
Akshat Gupta <akshatvg23@gmail.com>
2021-04-14 23:48:47 +0530

    day 3

--- agoraRtc.js
+++ agoraRtc.js
@@ -1,46 +1,55 @@
-let handlfail = function (err) {
-    console.log(err);
+let handlefail = function (err) {
+    console.log(err)
 }
 
-function addVideo(streamId) {
-    console.log("Adding Video");
+
+function addVideoStream(streamId) {
+    console.log()
     let remoteContainer = document.getElementById("remoteStream");
     let streamDiv = document.createElement("div");
     streamDiv.id = streamId;
-    streamDiv.style.height = "250px";
-    remoteContainer.appendChild(streamDiv);
+    // streamDiv.style.transform = "rotateY(180deg)";
+    streamDiv.style.height = "250px"
+    remoteContainer.appendChild(streamDiv)
 }
 
 document.getElementById("join").onclick = function () {
     let channelName = document.getElementById("channelName").value;
     let Username = document.getElementById("username").value;
-    let appId = "5f5688a419fc4e41ad5fe6450c7abfc2";
+    let appId = "a6af85f840ef43108491705e2315a857";
 
     let client = AgoraRTC.createClient({
-        mode: "rtc",
+        mode: "live",
         codec: "h264"
     })
 
-    client.init(appId, () => console.log("AgoraRTC Client Connected"), handlfail)
+    client.init(appId, () => console.log("AgoraRTC Client Connected"), handlefail
+    )
 
-    client.join(null, channelName, Username, () => {
-        var localStream = AgoraRTC.createStream({
-            video: true,
-            audio: true
-        })
-        localStream.init(function () {
-            localStream.play("SelfStream");
-            console.log(`App ID: ${appId}\nChannel Name: ${channelName}`)
-            client.publish(localStream);
-        })
-    })
+    client.join(
+        null,
+        channelName,
+        Username,
+        () => {
+            var localStream = AgoraRTC.createStream({
+                video: true,
+                audio: true,
+            })
 
-    client.on('stream-added', function (evt) {
+            localStream.init(function () {
+                localStream.play("SelfStream")
+                console.log(`App id: ${appId}\nChannel id: ${channelName}`)
+                client.publish(localStream)
+            })
+        }
+    )
+
+    client.on("stream-added", function (evt) {
         console.log("Added Stream");
-        client.subscribe(evt.stream, handlefail);
+        client.subscribe(evt.stream)
     })
 
-    client.on('stream-subscribed', function (evt) {
+    client.on("stream-subscribed", function (evt) {
         console.log("Subscribed Stream");
         let stream = evt.stream;
         addVideoStream(stream.getId());
